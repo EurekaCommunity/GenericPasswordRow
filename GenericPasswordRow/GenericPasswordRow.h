f17b7ed58c278def8e10826426e65cfb6c4d73e6
Diego Ernst <dernst@xmartlabs.com>
2016-09-05 16:29:48 -0300

    Initial Commit

---
+++ GenericPasswordRow/GenericPasswordRow.h
@@ -0,0 +1,19 @@
+//
+//  GenericPasswordRow.h
+//  GenericPasswordRow
+//
+//  Created by Diego Ernst on 9/5/16.
+//  Copyright © 2016 Diego Ernst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GenericPasswordRow.
+FOUNDATION_EXPORT double GenericPasswordRowVersionNumber;
+
+//! Project version string for GenericPasswordRow.
+FOUNDATION_EXPORT const unsigned char GenericPasswordRowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GenericPasswordRow/PublicHeader.h>
+
+
